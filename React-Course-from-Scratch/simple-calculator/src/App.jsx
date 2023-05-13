9dbf5cb75873a7756334baf8bb5ba9f9c81dba76
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-13 13:01:16 +0200

    useState adding

--- React-Course-from-Scratch/simple-calculator/src/App.jsx
+++ React-Course-from-Scratch/simple-calculator/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Calculator from './components/Calculator'
+import CalculatorUseReducer from './components/CalculatorUseReducer'
 
 function App() {
 
   return (
     <div className='App'>
       <Calculator />
+      <CalculatorUseReducer />
       
     </div>
   )
--- React-Course-from-Scratch/simple-calculator/src/components/Calculator.jsx
+++ React-Course-from-Scratch/simple-calculator/src/components/Calculator.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleNumber1Change = (e) => {
+    setNumber1(Number(e.target.value));
+  };
+
+  const handleNumber2Change = (e) => {
+    setNumber2(Number(e.target.value));
+  };
+
+  const handleAddition = () => {
+    setResult(number1 + number2);
+  };
+
   return (
     <div className="calculator-container">
       <h1>useState</h1>
@@ -9,20 +26,39 @@ const Calculator = () => {
           <label className="form-label" htmlFor="number1">
             Number 1
           </label>
-          <input className="form-input" type="number" id="number1" />
+          <input
+            className="form-input"
+            type="number"
+            id="number1"
+            onChange={handleNumber1Change}
+            value={number1}
+          />
         </div>
         <div className="form-group">
           <label className="form-label" htmlFor="number2">
             Number 2
           </label>
-          <input className="form-input" type="number" id="number2" />
+          <input
+            className="form-input"
+            type="number"
+            id="number2"
+            onChange={handleNumber2Change}
+            value={number2}
+          />
         </div>
-        <button className="form-button">Add</button>
+        <button className="form-button" onClick={handleAddition}>
+          Add
+        </button>
         <div className="form-group">
           <label className="form-label" htmlFor="result">
             Result
           </label>
-          <input className="form-input" type="number" id="result" />
+          <input
+            className="form-input"
+            type="number"
+            id="result"
+            value={result}
+          />
         </div>
       </div>
     </div>
