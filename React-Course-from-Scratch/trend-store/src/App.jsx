26e996fbdabda1601fd83c42f2f9d7be6f7bec23
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-15 07:18:22 +0200

    Hero Component

--- React-Course-from-Scratch/trend-store/src/App.jsx
+++ React-Course-from-Scratch/trend-store/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero/Hero";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ React-Course-from-Scratch/trend-store/src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="hero-title">
+        <span>Find the best this season</span>
+        <h2>Special collection for everyone</h2>
+        <a href="/">Discover Now</a>
+      </div>
+      <div className="hero-image-container">
+        <img className="hero-image" src="/public/images/hero.png" alt="hero" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
