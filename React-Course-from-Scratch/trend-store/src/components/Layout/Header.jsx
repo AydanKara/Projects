bb33bcf7b962c1c4206bee4efbf1eadbbc9ffbdc
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-15 06:35:02 +0200

    header component

--- React-Course-from-Scratch/trend-store/src/components/Layout/Header.jsx
+++ React-Course-from-Scratch/trend-store/src/components/Layout/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <h1>Trend Shop</h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
