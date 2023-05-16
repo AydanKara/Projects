d322c01ef16274031f5a3145aa95d4c4f812f3d7
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-16 07:21:34 +0200

    React Portal

--- React-Course-from-Scratch/trend-store/src/components/UI/Offcanvas.jsx
+++ React-Course-from-Scratch/trend-store/src/components/UI/Offcanvas.jsx
@@ -1,15 +1,34 @@
-import { Fragment } from "react"
-import "./Offcanvas.css"
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import "./Offcanvas.css";
+
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClose} />;
+};
+
+const OffcanvasOverlay = (props) => {
+  return (
+    <div className="offcanvas">
+      <div className="content">{props.children}</div>
+    </div>
+  );
+};
 
 const Offcanvas = (props) => {
+  const portalElement = document.getElementById("overlays");
   return (
     <Fragment>
-        <div className="backdrop" onClick={props.onClose} />
-        <div className="offcanvas">
-            <div className="content">{props.children}</div>
-        </div>
+      {ReactDOM.createPortal(
+        <OffcanvasOverlay>{props.children}</OffcanvasOverlay>,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      
     </Fragment>
-  )
-}
+  );
+};
 
-export default Offcanvas
+export default Offcanvas;
