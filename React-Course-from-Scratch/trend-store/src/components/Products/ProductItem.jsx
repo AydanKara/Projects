e76888274b12bdaa0e7622bbe77e4b97dcd31ae2
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-16 09:22:02 +0200

    add product to cart

--- React-Course-from-Scratch/trend-store/src/components/Products/ProductItem.jsx
+++ React-Course-from-Scratch/trend-store/src/components/Products/ProductItem.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 import Rating from "./Rating";
 import Card from "../UI/Card";
 import "./ProductItem.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
 
 const ProductItem = ({ product }) => {
   const { name, description, img, price } = product;
+  const { items, addItem, totalAmount } = useContext(CartContext);
+  console.log(totalAmount);
   return (
     <Card>
       <img src={img} alt={name} />
@@ -13,7 +18,9 @@ const ProductItem = ({ product }) => {
         <Rating />
         <span className="price">{price}$</span>
       </div>
-      <button className="add-to-cart">Add To Cart</button>
+      <button className="add-to-cart" onClick={() => addItem(product)}>
+        Add To Cart
+      </button>
     </Card>
   );
 };
--- React-Course-from-Scratch/trend-store/src/context/CartProvider.jsx
+++ React-Course-from-Scratch/trend-store/src/context/CartProvider.jsx
@@ -1,17 +1,51 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+        let updatedItems = [...state.items];
+        updatedItems = [...state.items, action.item];
+      return {
+        items: updatedItems,
+        totalAmount: state.totalAmount + action.item.price * action.item.amount,
+      };
+    case "REMOVE":
+      return state;
+    case "CLEAR":
+      return state;
+    default:
+      return state;
+  }
+};
+
 const CartProvider = ({ children }) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
   const cartContext = {
-    items: [],
-    totalAmount: 0,
-    addItem: () => {},
-    removeItem: () => {},
-    clearItem: () => {},
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: (item) => {
+      dispatchCartAction({ type: "ADD", item });
+    },
+    removeItem: () => {
+      dispatchCartAction({ type: "REMOVE" });
+    },
+    clearItem: () => {
+      dispatchCartAction({ type: "CLEAR" });
+    },
   };
-  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 };
 
 export default CartProvider;
