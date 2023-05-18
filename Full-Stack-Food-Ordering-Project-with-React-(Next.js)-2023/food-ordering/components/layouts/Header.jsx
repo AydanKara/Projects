5f7a09c08eae7d52662f6e8786fe0ada1d233362
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-18 12:25:25 +0200

    Click Outside update

--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/layouts/Header.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/layouts/Header.jsx
@@ -1,7 +1,12 @@
-import Logo from "../UI/Logo.jsx";
-import {FaUserAlt, FaShoppingCart, FaSearch} from "react-icons/fa"
+import { useState } from "react";
+import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+import OutsideClickHandler from "react-outside-click-handler";
+import Logo from "../UI/Logo";
+import Title from "../UI/Title";
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false);
+
   return (
     <div className="h-[5.5rem] bg-secondary">
       <div className="container mx-auto text-white flex justify-between items-center h-full">
@@ -9,34 +14,41 @@ const Header = () => {
         <nav>
           <ul className="flex gap-x-2">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-              <a href="/">Home</a>
+              <a href="">Home</a>
             </li>
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-              <a href="/">Menu</a>
+              <a href="">Menu</a>
             </li>
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-              <a href="/">About</a>
+              <a href="">About</a>
             </li>
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
-              <a href="/">Book Table</a>
+              <a href="">Book Table</a>
             </li>
           </ul>
         </nav>
         <div className="flex gap-x-4 items-center">
-            <a href="#">
-                <FaUserAlt />
-            </a>
-            <a href="#">
-                <FaShoppingCart />
-            </a>
-            <a href="#">
-                <FaSearch />
-            </a>
-            <a href="#">
-                <button className="btn-primary">Order Online</button>
-            </a>
+          <a href="#">
+            <FaUserAlt className="hover:text-primary transition-all" />
+          </a>
+          <a href="#">
+            <FaShoppingCart className="hover:text-primary transition-all" />
+          </a>
+          <button onClick={() => setIsSearchModal(true)}>
+            <FaSearch className="hover:text-primary transition-all" />
+          </button>
+          <a href="#">
+            <button className="btn-primary">Order Online</button>
+          </a>
         </div>
       </div>
+      {isSearchModal && (
+        <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
+          <div className="">
+            <Title addClass="text-9xl">Title</Title>
+          </div>
+        </OutsideClickHandler>
+      )}
     </div>
   );
 };
--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/home/index.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/home/index.jsx
@@ -1,7 +1,7 @@
 
 const Index = () => {
   return (
-    <div className="text-center font-dancing text-4xl"></div>
+    <div></div>
   )
 }
 
