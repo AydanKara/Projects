dc3fd351a270990b49f85498274ea49b1d28d612
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-21 21:41:35 +0200

    React Formik Setup

--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/Reservation.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/Reservation.jsx
@@ -1,54 +1,73 @@
 import React from "react";
 import Title from "./UI/Title";
 import Input from "./form/input";
+import { useFormik } from "formik";
 
 const Reservation = () => {
-
-    const inputs = [
-        {
-            id: 1,
-            name: "fullName",
-            type: "text",
-            placeholder: "Your Full Name",
-        },
-        {
-            id: 2,
-            name: "phoneNumber",
-            type: "number",
-            placeholder: "Your Phone Number",
-        },
-        {
-            id: 3,
-            name: "email",
-            type: "email",
-            placeholder: "Your Email",
-        },
-        {
-            id: 4,
-            name: "persons",
-            type: "number",
-            placeholder: "How many persons?",
-        },
-        {
-            id: 5,
-            name: "persons",
-            type: "datetime-local",
-            placeholder: "How many persons?",
-        },
-    ]
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    actions.resetForm();
+  }
+  const {values, handleSubmit, handleChange} = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      persons: "",
+      date: "",
+    },
+    onSubmit,
+  });
+  const inputs = [
+    {
+      id: 1,
+      name: "fullName",
+      type: "text",
+      placeholder: "Your Full Name",
+      value: values.fullName,
+    },
+    {
+      id: 2,
+      name: "phoneNumber",
+      type: "number",
+      placeholder: "Your Phone Number",
+      value: values.phoneNumber,
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Your Email",
+      value: values.email,
+    },
+    {
+      id: 4,
+      name: "persons",
+      type: "number",
+      placeholder: "How many persons?",
+      value: values.persons,
+    },
+    {
+      id: 5,
+      name: "date",
+      type: "datetime-local",
+      placeholder: "",
+      value: values.date,
+    },
+  ];
 
   return (
     <div className="container mx-auto py-12">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap gap-10">
-        <div className="lg:flex-1 w-full">
+        <form className="lg:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-                <Input key={input.id} {...input} />
+              <Input key={input.id} {...input} onChange={handleChange} />
             ))}
           </div>
-          <button className="btn-primary mt-4">BOOK NOW</button>
-        </div>
+          <button className="btn-primary mt-4" type="submit">BOOK NOW</button>
+        </form>
         <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17700.00173084974!2d-0.13925553616505992!3d51.50501018208041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20Vereinigtes%20K%C3%B6nigreich!5e0!3m2!1sde!2sde!4v1684692881836!5m2!1sde!2sde"
