1b8e61584da915b3e67812859037762b217039a5
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-21 12:45:57 +0200

    Menu Item Component

--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/product/MenuItem.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/product/MenuItem.jsx
@@ -1,10 +1,26 @@
 import Image from "next/legacy/image";
+import { FaShoppingCart } from "react-icons/fa";
 
 const MenuItem = () => {
   return (
-    <div>
-      <div className="relative w-36 h-36 z-50">
-        <Image src="/images/f1.png" alt="f1" layout="fill" />
+    <div className="bg-secondary rounded-[15px]">
+      <div className="w-full bg-[#f1f2f3] flex justify-center p-[25px] rounded-bl-[46px] rounded-t-[12px]">
+        <div className="relative w-36 h-36 hover:scale-110 transition-all">
+          <Image src="/images/f1.png" alt="f1" layout="fill" />
+        </div>
+      </div>
+      <div className="p-[25px] text-white">
+        <h4 className="text-xl font-semibold mb-2">Delicious Pizza</h4>
+        <p className="text-[15px] mb-4">
+          Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit,
+          magnam voluptatem repellendus sed eaque
+        </p>
+        <div className="flex justify-between items-center">
+          <span>$20</span>
+          <button className="btn-primary w-10 h-10 rounded-full !p-0 grid place-content-center">
+            <FaShoppingCart />
+          </button>
+        </div>
       </div>
     </div>
   );
