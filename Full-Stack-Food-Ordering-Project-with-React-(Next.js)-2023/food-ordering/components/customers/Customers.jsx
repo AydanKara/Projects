8e3371c55a26dd525a7eaf719d7379f50dd57d34
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-22 22:00:53 +0200

    Customers Component

---
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/components/customers/Customers.jsx
@@ -0,0 +1,16 @@
+import Title from "../UI/Title";
+import CustomerItem from "./CustomerItem";
+
+const Customers = () => {
+  return (
+    <div className="container mx-auto my-20">
+      <Title addClass="text-[40px] text-center">What Says Our Customers</Title>
+      <div className="flex gap-x-10">
+        <CustomerItem imgSrc="/images/client1.jpg" />
+        <CustomerItem imgSrc="/images/client2.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default Customers;
--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/home/index.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/home/index.jsx
@@ -1,18 +1,21 @@
+import React from "react"
 import Campaigns from "@/components/Campaigns"
 import Carousel from "@/components/Carousel"
 import About from "@/components/About"
 import MenuWrapper from "@/components/product/MenuWrapper"
 import Reservation from "@/components/Reservation"
+import Customers from "@/components/customers/Customers"
 
 const Index = () => {
   return (
-    <div>
+    <React.Fragment>
       <Carousel />
       <Campaigns />
       <MenuWrapper />
       <About />
       <Reservation />
-    </div>
+      <Customers />
+    </React.Fragment>
   )
 }
 
