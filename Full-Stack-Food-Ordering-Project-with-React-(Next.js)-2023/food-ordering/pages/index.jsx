5a420176bacdbb895f0a3868d927f6ab405065e1
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-05-25 19:45:00 +0200

    Footer Component

--- Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/index.jsx
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Home from "./home";
 import Header from "../components/layouts/Header";
 import Input from "@/components/form/input";
+import Footer from "@/components/layouts/Footer";
 
 export default function Index() {
   return (
@@ -20,6 +21,7 @@ export default function Index() {
       </Head>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
