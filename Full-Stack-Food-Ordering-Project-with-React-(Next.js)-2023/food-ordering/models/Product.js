e0315b92a225db8dc44570bfc1ec13944b4321bc
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-06-03 19:19:22 +0200

    Create Product.js

---
+++ Full-Stack-Food-Ordering-Project-with-React-(Next.js)-2023/food-ordering/models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxLength: 300,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    extraOptions: {
+      type: [
+        {
+          text: { type: String },
+          price: { type: Number },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
