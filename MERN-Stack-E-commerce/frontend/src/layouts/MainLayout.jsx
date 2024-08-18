65f12641be4bdaf9032bc4a07dbf129c486a55e2
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-18 15:29:29 +0200

    Add Dialog Component
    Add state to localStorage

--- MERN-Stack-E-commerce/frontend/src/layouts/MainLayout.jsx
+++ MERN-Stack-E-commerce/frontend/src/layouts/MainLayout.jsx
@@ -2,12 +2,26 @@ import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
 import PropTypes from "prop-types";
 import Search from "../components/Modals/Search/Search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
 const MainLayout = (props) => {
   const [isSearchShow, setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow] = useState(false);
+
+  useEffect(() => {
+    const dialogStatus = localStorage.getItem("dialog")
+      ? JSON.parse(localStorage.getItem("dialog"))
+      : localStorage.setItem("dialog", JSON.stringify(true));
+
+    setTimeout(() => {
+      setIsDialogShow(dialogStatus);
+    }, 2000);
+  }, []);
+
   return (
     <div className="main-layout">
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
       <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
       <Header setIsSearchShow={setIsSearchShow} />
       {props.children}
