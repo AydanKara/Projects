156adc78a0fa8c3004082ebacc60eb008f118970
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-23 11:14:31 +0200

    User API Endpoint
    Create User Model
    Create new User(Register)
    Using Random Avatar
    User Login

--- MERN-Stack-E-commerce/backend/routes/index.js
+++ MERN-Stack-E-commerce/backend/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const categoryRoute = require("./categories.js");
 const productsRoute = require("./products.js");
+const authRoute = require("./auth.js");
 
 router.use("/categories", categoryRoute);
 router.use("/products", productsRoute);
+router.use("/auth", authRoute);
 
 module.exports = router;
