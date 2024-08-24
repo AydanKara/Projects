539f4dcea8ff1c86bcd4f98e4f546100493af8e2
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-24 08:57:38 +0200

    Products API Endpoints
    Create Product Model
    CRUD Operations for Product

--- MERN-Stack-E-commerce/backend/routes/products.js
+++ MERN-Stack-E-commerce/backend/routes/products.js
@@ -1,9 +1,89 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
+
+// Create
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Read - Single
+router.get("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 // Read - All
-router.get("", async (req, res) => {
-  res.send("Get All Products");
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// Update
+router.put("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const updates = req.body;
+
+    const existingProduct = await Product.findById(productId);
+
+    if (!existingProduct) {
+      res.status(404).json({ error: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updates,
+      { new: true }
+    );
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+//! Delete
+
+router.delete("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    res.status(200).json(deletedProduct);
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
 module.exports = router;
