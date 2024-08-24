5b57da4be6a359d95425f768e029a90d1d875f51
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-24 11:36:02 +0200

    Create Admin Panel
    Create Admin Panel with Ant Design
    Create Admin Layout

--- MERN-Stack-E-commerce/frontend/src/layouts/AdminLayout.jsx
+++ MERN-Stack-E-commerce/frontend/src/layouts/AdminLayout.jsx
@@ -1,9 +1,177 @@
+import { Layout, Menu } from "antd";
+import PropTypes from "prop-types";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  RollbackOutlined,
+  BarcodeOutlined,
+  DashboardOutlined,
+  ShoppingCartOutlined,
+  AppstoreOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
+const { Sider, Header, Content } = Layout;
 
-const AdminLayout = () => {
+const AdminLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <DashboardOutlined />,
+      label: "Dashboard",
+      onClick: () => {
+        navigate(`/admin`);
+      },
+    },
+    {
+      key: "2",
+      icon: <AppstoreOutlined />,
+      label: "Categories",
+      path: "/",
+      children: [
+        {
+          key: "3",
+          label: "Category List",
+          path: "/admin/categories",
+          onClick: () => {
+            navigate(`/admin/categories`);
+          },
+        },
+        {
+          key: "4",
+          label: "Create new category",
+          path: "/admin/categories/create",
+          onClick: () => {
+            navigate("/admin/categories/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "5",
+      icon: <LaptopOutlined />,
+      label: "Products",
+      path: "/",
+      children: [
+        {
+          key: "6",
+          label: "Products list",
+          path: "/admin/products",
+          onClick: () => {
+            navigate(`/admin/products`);
+          },
+        },
+        {
+          key: "7",
+          label: "Create new product",
+          path: "/admin/products/create",
+          onClick: () => {
+            navigate("/admin/products/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "8",
+      icon: <BarcodeOutlined />,
+      label: "Coupons",
+      path: "/admin/coupons",
+      children: [
+        {
+          key: "9",
+          label: "Coupons list",
+          path: "/admin/coupons",
+          onClick: () => {
+            navigate(`/admin/coupons`);
+          },
+        },
+        {
+          key: "10",
+          label: "Create new coupon",
+          path: "/admin/coupons/create",
+          onClick: () => {
+            navigate("/admin/coupons/create");
+          },
+        },
+      ],
+    },
+    {
+      key: "11",
+      icon: <UserOutlined />,
+      label: "User List",
+      path: "/admin/users",
+      onClick: () => {
+        navigate(`/admin/users`);
+      },
+    },
+    {
+      key: "12",
+      icon: <ShoppingCartOutlined />,
+      label: "Orders",
+      onClick: () => {
+        navigate(`/admin/orders`);
+      },
+    },
+    {
+      key: "13",
+      icon: <RollbackOutlined />,
+      label: "Go to Home Page",
+      onClick: () => {
+        navigate(`/`);
+      },
+    },
+  ];
+
   return (
-    <div>AdminLayout</div>
-  )
-}
+    <div className="admin-layout">
+      <Layout
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Sider width={200} theme="dark">
+          <Menu
+            mode="vertical"
+            style={{
+              height: "100%",
+            }}
+            items={menuItems}
+          />
+        </Sider>
+        <Layout>
+          <Header>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                color: "white",
+              }}
+            >
+              <h2>Admin Panel</h2>
+            </div>
+          </Header>
+          <Content>
+            <div
+              className="site-layout-background"
+              style={{
+                padding: "24px 50px",
+                minHeight: 360,
+              }}
+            >
+              {children}
+              asd
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
+
+AdminLayout.propTypes = {
+  children: PropTypes.node,
+};
--- MERN-Stack-E-commerce/frontend/src/main.jsx
+++ MERN-Stack-E-commerce/frontend/src/main.jsx
@@ -1,18 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import CartProvider from "./context/CartProvider.jsx";
+import { Layout } from "./layouts/Layouts.jsx";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom"
-import MainLayout from "./layouts/MainLayout.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
-import CartProvider from "./context/CartProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <MainLayout>
+      <Layout>
         <App />
-      </MainLayout>
+      </Layout>
     </CartProvider>
   </BrowserRouter>
 );
