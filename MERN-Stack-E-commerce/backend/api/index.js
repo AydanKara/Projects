eded58ff567242e3db980e01a09e940298592b73
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-30 22:49:52 +0200

    Update index.js

--- MERN-Stack-E-commerce/backend/api/index.js
+++ MERN-Stack-E-commerce/backend/api/index.js
@@ -1,29 +1,3 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const logger = require("morgan");
-const mainRoute = require("../routes/index.js");
-
-dotenv.config();
-
-const connectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    throw new Error("Couldn't connect to MongoDB");
-  }
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello from Vercel!" });
 };
-
-const app = express();
-
-// middlewares
-app.use(logger("dev"));
-app.use(express.json());
-app.use(cors());
-
-app.use("/api", mainRoute);
-
-connectDatabase();
-module.exports = app;
