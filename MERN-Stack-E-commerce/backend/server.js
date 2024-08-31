e0ca2cd5fdb97f0001fa92410525afffa0e483ff
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-08-31 08:32:35 +0200

    Update server.js

--- MERN-Stack-E-commerce/backend/server.js
+++ MERN-Stack-E-commerce/backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = express();
 const cors = require("cors");
 const logger = require("morgan");
-const mainRoute = require("./routes/index.js");
-const port = 5000;
+const mainRoute = require("../routes/index.js");
 
 dotenv.config();
 
@@ -14,18 +12,19 @@ const connectDatabase = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
+    console.error("Couldn't connect to MongoDB", error);
     throw new Error("Couldn't connect to MongoDB");
   }
 };
+
+const app = express();
 
 // middlewares
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
-
 app.use("/api", mainRoute);
 
-app.listen(port, () => {
-  connectDatabase();
-  console.log(`Server listening on ${port}`);
-});
+connectDatabase();
+
+module.exports = app;
